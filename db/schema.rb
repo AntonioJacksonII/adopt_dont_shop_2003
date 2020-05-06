5f3b1cc014d979ee8b9b00fb0815b2ae4eba3dc8
Antonio Jackson <antoniojackson@gmail.com>
2020-05-05 19:19:09 -0600

    Schema updated with CreatePets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505234135) do
+ActiveRecord::Schema.define(version: 20200506011254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "sex"
+    t.string "shelter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
