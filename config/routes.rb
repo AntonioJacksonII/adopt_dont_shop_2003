260abfb4537305b6a1463590af0caddb105d70b3
Antonio Jackson <antoniojackson@gmail.com>
2020-05-06 17:09:10 -0600

    Adding all of the RESTful routes for shelters and pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  get '/shelters', to: 'shelters#index'
-  get '/pets', to: 'pets#index'
+  get "/shelters", to: "shelters#index"
+  get "/shelters/:id", to: "shelters#show"
+  get "/shelters/new", to: "shelters#new"
+  post "/shelters", to: "shelters#create"
+  get "/shelters/:id/edit", to: "shelters#edit"
+  put "/shelters/:id", to: "shelters#update"
+  delete "/posts/:id", to: "posts#destroy"
+
+  get "/pets", to: "pets#index"
+  get "/pets/:id", to: "pets#show"
+  get "/pets/new", to: "pets#new"
+  post "/pets", to: "pets#create"  # usually a submitted form
+  get "/pets/:id/edit", to: "pets#edit"
+  put "/pets/:id", to: "pets#update" # usually a submitted form
+  delete "/pets/:id", to: "pets#destroy"
 end
