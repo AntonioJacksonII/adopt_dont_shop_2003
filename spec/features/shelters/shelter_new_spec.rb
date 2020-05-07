d530e14ffe163d9beb273cc39e41d1c2101d2567
Antonio Jackson <antoniojackson@gmail.com>
2020-05-07 07:55:20 -0600

    Adding a test of the Create Shelter button and redirect to the shelter new spec features test

--- spec/features/shelters/shelter_new_spec.rb
+++ spec/features/shelters/shelter_new_spec.rb
@@ -7,4 +7,15 @@ describe "shelter new page", type: :feature do
 
     expect(page).to have_button("Create Shelter")
   end
+
+  it "redirects to the shelter index page" do
+    visit "/shelters/new"
+    fill_in "shelter[name]", with: "Shelter A"
+    fill_in "shelter[address]", with: "123 Main St"
+    fill_in "shelter[city]", with: "Denver"
+    fill_in "shelter[state]", with: "CO"
+    fill_in "shelter[zip]", with: "80202"
+    click_button "Create Shelter"
+    expect(current_path).to eq("/shelters")
+  end
 end
