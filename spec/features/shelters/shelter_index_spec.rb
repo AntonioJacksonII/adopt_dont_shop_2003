9c35dfc39073dfa7b25899fb2135ce4f9ac26bf9
Antonio Jackson <antoniojackson@gmail.com>
2020-05-07 05:28:24 -0600

    Adding models specs for validations and relationships to pets and shelters

--- spec/features/shelters/shelter_index_spec.rb
+++ spec/features/shelters/shelter_index_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
   it "can see the name of each shelter in the system" do
-    shelter_1 = Shelter.create(name: "Pablo's Puppies")
-    shelter_2 = Shelter.create(name: "Matt's Mutts")
+    shelter_1 = Shelter.create(name: "Pablo's Puppies",
+                              address: "123 Main St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80202")
+    shelter_2 = Shelter.create(name: "Matt's Mutts",
+                              address: "321 Niam St",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80001")
 
     visit "/shelters"
 
