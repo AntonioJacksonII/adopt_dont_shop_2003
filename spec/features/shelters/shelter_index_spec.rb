4e3ec99dffcbf890e88c2b986a884d4d26432e86
Antonio Jackson <antoniojackson@gmail.com>
2020-05-10 10:13:33 -0600

    Adding Shelter Links functionality to the Shelter Index page

--- spec/features/shelters/shelter_index_spec.rb
+++ spec/features/shelters/shelter_index_spec.rb
@@ -25,4 +25,22 @@ RSpec.describe "shelter index page", type: :feature do
     click_link "New Shelter"
     expect(current_path).to eq("/shelters/new")
   end
+
+  it "has links on each shelter name that take you to that shelter's show page" do
+    shelter_1 = Shelter.create(name: "Pablo's Puppies",
+                              address: "123 Main St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80202")
+    shelter_2 = Shelter.create(name: "Matt's Mutts",
+                              address: "321 Niam St",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80001")
+    visit "/shelters"
+    expect(page).to have_link("Pablo's Puppies")
+    expect(page).to have_link("Matt's Mutts")
+    click_link "Pablo's Puppies"
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+  end
 end
