652b47e1939864d49be5ee9405e5901d660a710c
Antonio Jackson <antoniojackson@gmail.com>
2020-05-10 15:10:01 -0600

    Adding Pet Index link to Pets Index page

--- spec/features/pets/pet_index_spec.rb
+++ spec/features/pets/pet_index_spec.rb
@@ -132,4 +132,27 @@ RSpec.describe "pet index page", type: :feature do
     click_link "Pablo's Puppies"
     expect(current_path).to eq("/shelters/#{shelter_1.id}")
   end
+
+  it "has a link to the Pet index" do
+    shelter_1 = Shelter.create(name: "Pablo's Puppies",
+                              address: "123 Main St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80202")
+    pet_1 = Pet.create( image: "/img/fido.jpg",
+                        name: "Fido",
+                        age: 4,
+                        sex: "male",
+                        shelter: shelter_1,
+                        description: "cute puppy",
+                        adoption_status: "adoptable")
+
+    visit "/pets"
+
+    expect(page).to have_link("Pet Index")
+
+    click_link "Pet Index"
+
+    expect(current_path).to eq("/pets")
+  end
 end
