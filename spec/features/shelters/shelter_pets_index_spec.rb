cdfdc787cab6e8bc8ce5c9325c5129d281236dc4
Antonio Jackson <antoniojackson@gmail.com>
2020-05-10 14:34:01 -0600

    Adding Edit Pet link for each pet on the Shelter Pets Index page

--- spec/features/shelters/shelter_pets_index_spec.rb
+++ spec/features/shelters/shelter_pets_index_spec.rb
@@ -51,4 +51,24 @@ describe "Shelter Pets Index Page", type: :feature do
       click_link("Create Pet")
       expect(current_path).to eq("/shelters/#{shelter_1.id}/pets/new")
   end
+
+  it "has links next to each pet to edit that pet" do
+      shelter_1 = Shelter.create(name: "Pablo's Puppies",
+                                address: "123 Main St",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+      pet_1 = Pet.create( image: "/img/fido.jpg",
+                      name: "Fido",
+                      age: 4,
+                      sex: "male",
+                      shelter: shelter_1,
+                      description: "cute puppy",
+                      adoption_status: "adoptable")
+
+      visit "/shelters/#{shelter_1.id}/pets"
+      expect(page).to have_link("Edit Pet")
+      click_link("Edit Pet")
+      expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+  end
 end
