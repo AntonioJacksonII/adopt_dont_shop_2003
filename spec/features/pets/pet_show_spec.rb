17c4350247329cfecdd8453b91646fa6a37297c0
Antonio Jackson <antoniojackson@gmail.com>
2020-05-10 14:55:30 -0600

    Adding link to the Pet Show page to the pet's name on the Pet Show page

--- spec/features/pets/pet_show_spec.rb
+++ spec/features/pets/pet_show_spec.rb
@@ -46,4 +46,25 @@ describe "pet show page", type: :feature do
     click_link "Update Pet"
     expect(current_path).to eq("/pets/#{pet_1.id}/edit")
   end
+
+  it "has a link on the pet's name to the pet's show page" do
+    shelter_1 = Shelter.create(name: "Pablo's Puppies",
+                              address: "123 Main St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80202")
+    pet_1 = Pet.create( image: "/img/fido.jpg",
+                        name: "Fido",
+                        description: "Cute puppy",
+                        age: 4,
+                        sex: "male",
+                        shelter: shelter_1,
+                        adoption_status: "adoptable")
+
+    visit "/pets/#{pet_1.id}"
+
+    expect(page).to have_link("Fido")
+    click_link "Fido"
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
 end
